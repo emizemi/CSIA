33fa96a2146396c9e690d80a54e6824f260bbe7f
emizemi <encar7439@gapps.uwcsea.edu.sg>
2018-07-23 18:19:07 +0800

    Started the app, inserted the table views and tab bar controller.

---
+++ CSIA/DisplayReflections.swift
@@ -0,0 +1,45 @@
+//
+//  DisplayReflections.swift
+//  CSIA
+//
+//  Created by Emilio Encarnacion on 23/7/18.
+//  Copyright © 2018 Emilio Encarnacion. All rights reserved.
+//
+
+import UIKit
+
+class DisplayReflections: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
