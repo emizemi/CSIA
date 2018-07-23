82f313e103d4fa8430f25278bb544840cec33133
emizemi <encar7439@gapps.uwcsea.edu.sg>
2018-07-23 16:35:56 +0800

    Initial Commit

---
+++ CSIA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CSIA
+//
+//  Created by Emilio Encarnacion on 23/7/18.
+//  Copyright © 2018 Emilio Encarnacion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
